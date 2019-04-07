373ce2768c32bada13a65e21f8f276a2f49c88e4
qing <15711217847@136.com>
2019-04-07 15:56:11 +0800

    极光

---
+++ app/src/main/java/com/bwie/asus/jiguangdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bwie.asus.jiguangdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/bwie/asus/jiguangdemo/MyApp.java
@@ -0,0 +1,15 @@
+package com.bwie.asus.jiguangdemo;
+
+import android.app.Application;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+    }
+}
